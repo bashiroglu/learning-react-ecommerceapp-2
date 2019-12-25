82d13246ffe91a7af7e6246a2e2a065d485ce293
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-25 21:00:41 +0100

    added homepage component and styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Homepage from './Homepage.component';
 import './App.css';
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
