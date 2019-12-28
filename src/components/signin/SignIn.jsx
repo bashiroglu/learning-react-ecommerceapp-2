c4ac57236e386643e5979fccd9db0e58f009ee1c
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-28 19:41:46 +0100

    added funct of signing

--- src/components/signin/SignIn.jsx
+++ src/components/signin/SignIn.jsx
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 import FormInput from '../custominput/CustomInput';
 import CustomButton from '../custombutton/CustomButton';
 
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './signin.scss';
+
 
 class SignIn extends Component {
   constructor(props) {
@@ -17,9 +18,18 @@ class SignIn extends Component {
     };
   }
 
-  handleSubmit = e => {
+  handleSubmit = async e => {
     e.preventDefault();
-    this.setState({ email: '', password: '' });
+
+    const { email, password } = this.state;
+    try {
+      /* signInWithEmailAndPassword we use this method from auth to sign in, it handle rest */
+      await auth.signInWithEmailAndPassword(email, password);
+      /* this is to clean our imputs */
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = e => {
@@ -45,14 +55,13 @@ class SignIn extends Component {
           <FormInput
             name="password"
             type="password"
-            value={this.state.email}
+            value={this.state.password}
             handleChange={this.handleChange}
             label="password"
             required
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign in </CustomButton>
-
             <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
               Sign in with Google
             </CustomButton>
