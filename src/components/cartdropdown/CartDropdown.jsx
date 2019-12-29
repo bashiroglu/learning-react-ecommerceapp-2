c40950ba78a7b9fc70261f90fdc81bef5b0497e6
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-29 16:10:44 +0100

    add funct of displaying cart items via data comes from redux

--- src/components/cartdropdown/CartDropdown.jsx
+++ src/components/cartdropdown/CartDropdown.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
+import CartItem from '../cartitem/CartItem';
 import CustomButton from '../custombutton/CustomButton';
 
 import './cart-dropdown.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className="cart-dropdown">
-    <div className="cart-items" />
+    <div className="cart-items">
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
 
-export default CartDropdown;
+export default connect(mapStateToProps)(CartDropdown);
