8d02599ea2e8f241db795edc2141cbe3fc4f197f
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-30 17:36:08 +0100

    we added stripe button comp via  react-stripe-checkout and finished front side of stripe intigration

---
+++ src/components/stripebutton/StripeButton.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;/* it needs the money with cents */
+  /* this is publishable key which stripe provide for us and
+  detect us when we use */
+  const publishableKey = 'pk_test_RcPAcPkum7xDNCSudpvJkBpt00TZtUK36p';
+
+  const onToken = token => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
