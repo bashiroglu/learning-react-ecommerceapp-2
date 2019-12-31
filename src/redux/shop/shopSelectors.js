56aa7de7d7a367c2901b54aa6083a81d2cd6889b
Abdulla Bashiroglu <abdullabashir32@gmail.com>
2019-12-31 11:34:55 +0100

    added l. commments and f. to the reselect shop file before data normalization

--- src/redux/shop/shopSelectors.js
+++ src/redux/shop/shopSelectors.js
@@ -16,3 +16,21 @@ export const selectCollectionsForPreview = createSelector(
   [selectCollections],
   collections => Object.keys(collections).map(key => collections[key])
 );
+
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  men: 5
+};
+
+export const selectCollectionWithId = collectionUrlParam =>
+  createSelector([selectCollections], collections =>
+    collections.find(
+      /* before data normalization we used to obtain collection by using this method */
+      /* what we basically say, in collections item find the collection 
+      where its id equals to value of property of collectionUrlParam in COLLECTION_ID_MAP */
+      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+    )
+  );
